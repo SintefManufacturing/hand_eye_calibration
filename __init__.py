88c304c5e2ab99464d24b1eed83da4f6fcce60ac
Morten Lind <morten@lind.dyndns.dk>
2017-09-07 14:58:14 +0200

    Initial commit based on SINTEF Stash code.

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+"""
+"""
+
+__author__ = "Morten Lind"
+__copyright__ = "SINTEF 2017"
+__credits__ = ["Morten Lind"]
+__license__ = "GPLv3"
+__maintainer__ = "Morten Lind"
+__email__ = "morten.lind@sintef.no"
+__status__ = "Development"
+
+from .park_martin_calibration import ParkMartinCalibrator
